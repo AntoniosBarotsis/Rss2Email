e20a05ace851b4119745491c209814c9a5a42473
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-09-26 11:15:43 +0200

    Refactored email providers with enum dispatch

--- src/email/email_provider.rs
+++ src/email/email_provider.rs
@@ -1,35 +1,41 @@
 use super::sendgrid::SendGrid;
+use enum_dispatch::enum_dispatch;
 use log::warn;
 
 /// An email provider abstraction to allow for multiple backends.
+#[enum_dispatch]
 pub trait EmailProvider {
   /// Sends an email to and from the specified address.
   fn send_email(&self, address: &str, api_key: &str, contents: &str);
 }
 
+/// An enum containing all Email Provider implementations.
+#[enum_dispatch(EmailProvider)]
+enum EmailProviders {
+  SendGrid(SendGrid),
+}
+
+impl From<String> for EmailProviders {
+  fn from(input: String) -> Self {
+    // Note that the input is trimmed and converted
+    // to upper case for the sake of consistency
+    match input.trim().to_uppercase().as_str() {
+      "SENDGRID" => Self::SendGrid(SendGrid::new()),
+      e => {
+        warn!("Invalid Email provider: {}, defaulting to SendGrid.", e);
+        Self::SendGrid(SendGrid::new())
+      }
+    }
+  }
+}
+
 /// Abstracts away the email backend.
 ///
-/// Currently only Sendgrid is implemented but in the future,
-/// the implementation choice will be made here.
-///
-/// The env arg is trimmed and converted to uppercase for the
-/// sake of consistency before matching it.
-///
 /// By default, this will return the `SendGrid` implementation.
-/// 
-/// New `EmailProvider` implementations must be added here, otherwise
-/// they will be ignored.
 pub fn get_email_provider() -> impl EmailProvider {
   let env_var = std::env::var("EMAIL")
     .ok()
     .unwrap_or_else(|| "SENDGRID".to_owned());
 
-  #[allow(clippy::match_single_binding)]
-  match env_var.trim().to_uppercase().as_str() {
-    "SENDGRID" => SendGrid::new(),
-    e => {
-      warn!("Invalid Email provider: {}, defaulting to SendGrid.", e);
-      SendGrid::new()
-    }
-  }
+  EmailProviders::from(env_var)
 }
--- src/email/mod.rs
+++ src/email/mod.rs
@@ -1,3 +1,4 @@
 #[allow(clippy::module_name_repetitions)]
+#[allow(clippy::use_self)]
 pub mod email_provider;
 pub mod sendgrid;
