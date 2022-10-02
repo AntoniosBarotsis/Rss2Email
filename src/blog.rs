6bf227cd6a1a2455ded4c2960416d7a7ad7ea3af
Kineolyan <kineolyan@protonmail.com>
2022-10-02 14:31:43 +0200

    Setup the first tests of parsing.

--- src/blog.rs
+++ src/blog.rs
@@ -1,13 +1,13 @@
 use chrono::{DateTime, FixedOffset};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Blog {
   pub title: String,
   pub last_build_date: DateTime<FixedOffset>,
   pub posts: Vec<Post>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Post {
   pub title: String,
   pub link: String,
--- src/xml/mod.rs
+++ src/xml/mod.rs
@@ -23,3 +23,89 @@ pub fn parse_web_feed(xml: &str) -> Result<Blog, String> {
     .cloned()
     .ok_or_else(|| format!("{:?}", errors.iter().unique().collect::<Vec<_>>()))
 }
+
+#[cfg(test)]
+mod tests {
+  use super::parse_web_feed;
+  use crate::blog::{Blog, Post};
+  use chrono::{DateTime, FixedOffset};
+
+  fn read_file(dir_name: &str, file_name: &str) -> String {
+    use std::fs;
+    use std::path::PathBuf;
+    let mut file_path = PathBuf::from("test-data");
+    file_path.push(dir_name);
+    file_path.push(file_name);
+
+    fs::read_to_string(file_path).expect("Cannot read feed ")
+  }
+
+  fn read_rss(file_name: &str) -> String {
+    read_file("rss-feeds", file_name)
+  }
+
+  fn read_atom(file_name: &str) -> String {
+    read_file("atom-feeds", file_name)
+  }
+
+  fn post_date(value: &str) -> DateTime<FixedOffset> {
+    value
+      .parse::<DateTime<FixedOffset>>()
+      .expect(&format!("Invalid date {}", value))
+  }
+
+  #[test]
+  fn test_parse_rss_data() {
+    let content = read_rss("rss.xml");
+    let blog = parse_web_feed(&content).expect("Parsed content");
+    println!("{:?}", blog);
+  }
+
+  #[test]
+  fn test_parse_brief_single_entry_atom() {
+    let content = read_atom("brief-single-entry.xml");
+    let blog = parse_web_feed(&content).expect("Parsed content");
+
+    assert_eq!(
+      blog,
+      Blog {
+        title: "Example Feed".into(),
+        last_build_date: post_date("2003-12-13T18:30:02+00:00"),
+        posts: vec![Post {
+          title: "Atom-Powered Robots Run Amok".into(),
+          link: "http://example.org/2003/12/13/atom03".into(),
+          description: Some("Some text.".into()),
+          last_build_date: post_date("2003-12-13T18:30:02+00:00"),
+        }],
+      }
+    );
+  }
+  #[test]
+  fn test_parse_complex_single_entry_atom() {
+    let content = read_atom("complex-single-entry.xml");
+    let blog = parse_web_feed(&content).expect("Parsed content");
+
+    // The chosen link is the .../2005/04/02/atom because it is the first in the list
+    assert_eq!(
+      blog,
+      Blog {
+        title: "dive into mark".into(),
+        last_build_date: post_date("2005-07-31T12:29:29+00:00"),
+        posts: vec![Post {
+          title: "Atom draft-07 snapshot".into(),
+          link: "http://example.org/2005/04/02/atom".into(),
+          description: None,
+          last_build_date: post_date("2005-07-31T12:29:29+00:00"),
+        }],
+      });
+  }
+
+  #[test]
+  fn test_parse_feed_without_entry() {}
+
+  #[test]
+  fn test_parse_feed_with_many_entries() {}
+
+  // Ignored:
+  // - multiple feeds in a single document
+}
