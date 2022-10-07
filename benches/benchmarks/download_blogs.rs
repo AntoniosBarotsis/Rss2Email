484e7bf74e3411fad407b5f39b7e7381e599f87e
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-10-07 14:01:58 +0200

    Removed redundant group

--- benches/benchmarks/download_blogs.rs
+++ benches/benchmarks/download_blogs.rs
@@ -2,9 +2,7 @@ use criterion::{criterion_group, Criterion};
 use rss2email::download_blogs;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-  let mut group = c.benchmark_group("download blogs");
-  group.bench_function("download blogs", |f| f.iter(|| download_blogs(1)));
-  group.finish();
+  c.bench_function("download blogs", |b| b.iter(|| download_blogs(7)));
 }
 
 criterion_group! {
