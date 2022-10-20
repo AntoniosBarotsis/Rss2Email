1666bf210ca7059c9301b357ca81ca0679c7dc59
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-10-21 00:53:20 +0200

    to back to sh

--- test-docker/build.sh
+++ test-docker/build.sh
@@ -14,6 +14,4 @@ docker run \
   -e "EMAIL_ADDRESS"="root@localhost" \
   -e "FEEDS"="https://blog.rust-lang.org/feed.xml;" \
   -e "DAYS"="50" \
-  debian-email:test sh -c "rss2email && sleep 5 && cat /var/mail/mails"
-
-rm rss2email
+  debian-email:test sh -c "rss2email && sleep 5 && cat /var/mail/mail"
