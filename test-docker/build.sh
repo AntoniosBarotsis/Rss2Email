93ebf1f2e25d33c9df642186c5cb9cd8f9a80f7d
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-10-20 21:59:10 +0200

    Fail on purpose

--- test-docker/build.sh
+++ test-docker/build.sh
@@ -14,6 +14,6 @@ docker run \
   -e "EMAIL_ADDRESS"="root@localhost" \
   -e "FEEDS"="https://blog.rust-lang.org/feed.xml;" \
   -e "DAYS"="50" \
-  debian-email:test sh -c "rss2email && sleep 5 && cat /var/mail/mail"
+  debian-email:test sh -c "rss2email && sleep 5 && cat /var/mail/mails"
 
 rm rss2email
