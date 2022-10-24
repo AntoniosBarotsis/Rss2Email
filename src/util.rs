0d964deb57a364bc3099b9d73275048ce7cdd173
oliviacarino <olcarino@gmail.com>
2022-10-24 19:55:54 -0400

    added prototype for simple html data. html is still dumped in terminal and not stored in a file yet. this is a very simple implementation right now.

--- src/util.rs
+++ src/util.rs
@@ -91,6 +91,22 @@ fn read_feeds(feed_flag: usize, env_links: Vec<&str>) -> Vec<String> {
 
 /// Generates the HTML contents corresponding to the given Blog collection.
 pub fn map_to_html(blogs: &Vec<Blog>) -> String {
+  let styling_part1 = format!("
+    <!DOCTYPE html>
+    <html lang=\"en\">
+        <head>
+            <meta charset=\"utf-8\" />
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\" />
+            <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">
+        </head>
+        <body>
+            <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark text-light\">
+              <a class=\"navbar-brand\">Rss2Email Feed</a>
+            </nav>
+            <div class=\"container\">
+                <div class=\"text-left mt-5\">
+  ");
+  
   let mut res = format!("<h1>Rss2Email - {}</h1>", Utc::now().date());
 
   for blog in blogs {
@@ -102,7 +118,23 @@ pub fn map_to_html(blogs: &Vec<Blog>) -> String {
     res.push_str(&tmp);
   }
 
-  res
+  let styling_part2: String = format!("
+      </div>
+      </div>
+      
+      <!-- Bootstrap core JS-->
+      <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\"></script>
+    </body>
+    </html>
+  ");
+
+  let mut res2: String = format!("");
+  res2.push_str(&styling_part1);
+  res2.push_str(&res);
+  res2.push_str(&styling_part2);
+
+  res2
+  //res
 }
 
 /// Returns true if the passed date is within `n` days from the current date.
