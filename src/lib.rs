679fbcd89392c49e3349bcad24d6a6138dad8e9f
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-07-10 05:57:38 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -191,7 +191,7 @@ pub async fn get_page_async(url: &str, client: &Client) -> Result<String, Error>
   }
 
   if !response.status().is_success() {
-    return Err(Error::Generic(response.text().await?))
+    return Err(Error::Generic(response.text().await?));
   }
 
   response
